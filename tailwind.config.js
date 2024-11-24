29cd986fd68b8454c4a5225049f5e1a64f33d9ca
CazzVicco-rbg <Cazz.vicco@gmail.com>
2024-11-24 16:28:40 -0300

    Upload projeto

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'C': '0 20px 10px #0000004d',
+      }
+    },
+  },
+  plugins: [],
+  
+
+}
